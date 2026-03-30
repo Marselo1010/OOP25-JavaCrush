6406482e886c88beef6aac16d9951dda279a85b2
Marco Galli <marco.galli14@studio.unibo.it>
2026-03-30 19:21:30 +0200

    created first 3 methods of GameController

--- src/main/java/it/unibo/javacrush/controller/api/GameController.java
+++ src/main/java/it/unibo/javacrush/controller/api/GameController.java
@@ -1,10 +1,33 @@
 package it.unibo.javacrush.controller.api;
 
+import it.unibo.javacrush.common.Position;
+
 /**
  * Interface representing a game controller.
  * This interface defines the contract for controlling the game logic,
  * including handling user input, updating the game state, and managing the game flow.
 */
 public interface GameController {
+    
+    /**
+     * The method responsible of the move making by swapping two cells.
+     * 
+     * @param p1 the first selected cell.
+     * @param p2 the second selected cell.
+     */
+    void makeMove(Position p1, Position p2);
 
+    /**
+     * Returns if the player had loose the game.
+     * 
+     * @return true if the player run out of moves, false otherwise.
+     */
+    boolean isGameOver();
+
+    /**
+     * Return if the player had won.
+     * 
+     * @return true if the player had achieved all the objectives, false otherwise.
+     */
+    boolean isWin();
 }
